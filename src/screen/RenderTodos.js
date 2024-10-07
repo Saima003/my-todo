f2afe7dc1676f498d240d57367d0e188d794b455
Saima Khan <saima19@somaiya.edu>
2024-10-07 20:45:49 +0530

    feat: todo add and add handler

---
+++ src/screen/RenderTodos.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RenderTodos = () => {
+  return (
+    <View>
+      <Text>RenderTodos</Text>
+    </View>
+  )
+}
+
+export default RenderTodos
+
+const styles = StyleSheet.create({})
--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,18 +1,30 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity,SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView, ToastAndroid } from 'react-native'
+import { React, useState } from 'react'
 const TodoScreen = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList , setTodoList] = useState([])
+  const addHandler =()=>{
+    if(todo === ""){
+      ToastAndroid.showWithGravity(
+        'Please Enter a Task',
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+      );
+    }
+  }
   return (
-    <View >
-      <TextInput  style={{borderWidth: 2, borderColor : "#1e90ff", borderRadius: 6, paddingVertical : 6,
+    <View style={{ marginTop: 20, marginHorizontal: 10 }}>
+      <TextInput style={{
+        borderWidth: 2, borderColor: "#1e90ff", borderRadius: 6, paddingVertical: 6,
         paddingHorizontal: 16,
-      }} placeholder="Add a Task"/>
-    <TouchableOpacity style={{backgroundColor: "#000", borderRadius: 6, paddingVertical:12}}>
-        <Text style={{color: "white"}}>Add</Text>
-    </TouchableOpacity>
+      }} placeholder="Add a Task" value={todo} onChangeText={text => setTodo(text)} />
+      <TouchableOpacity style={{ backgroundColor: "#000", borderRadius: 6, marginVertical: 16, paddingVertical: 12, justifyContent: "center" }} onPress={e => addHandler()}>
+        <Text style={{ color: "white", fontSize: 16, textAlign: 'center' }}>Add</Text>
+      </TouchableOpacity>
     </View>
   )
 }
- 
+
 export default TodoScreen
 
 const styles = StyleSheet.create({})
